6cef4bfc7aca2e99652eddcea5188ed28d06427f
kamonsak <kamonsak.ba.64@ubu.ac.th>
2023-11-10 00:04:53 +0700

    แก้ base manage interiver

---
+++ interview/static/js/admin_page/TemporaryUser.js
@@ -0,0 +1,8 @@
+function add_TemporaryUser() {
+    var optionsDiv = document.getElementById("add_TemporaryUser");
+    if (optionsDiv.style.display === "block") {
+      optionsDiv.style.display = "none";
+    } else {
+      optionsDiv.style.display = "block";
+    }
+  }
