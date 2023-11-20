c4fd1d27a66bce187ae77ca03ba0614914e4a349
kamonsak <kamonsak.ba.64@ubu.ac.th>
2023-11-20 14:29:28 +0700

    แก้ html css  ข้อมูลส่วนตัว

---
+++ interview/static/js/admin_page/admin_profile.js
@@ -0,0 +1,8 @@
+function changepassword() {
+    var optionsDiv = document.getElementById("changepassword");
+    if (optionsDiv.style.display === "block") {
+      optionsDiv.style.display = "none";
+    } else {
+      optionsDiv.style.display = "block";
+    }
+  }
