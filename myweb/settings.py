1ad77e1524063bc27838e3b96adcb932bf0dc390
kamonsak <kamonsak.ba.64@ubu.ac.th>
2024-01-16 19:07:20 +0700

    ทดลองอัพ

--- myweb/settings.py
+++ myweb/settings.py
@@ -98,19 +98,19 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-    
-    # "default": {
-    #    'ENGINE':'django.db.backends.mysql',
-    #    "NAME": os.environ.get("DB_NAME"),
-    #    "USER": os.environ.get("DB_USER"),
-    #     "PASSWORD": os.environ.get("DB_PASS"),
-    #     "HOST": os.environ.get("DB_HOST"),
-    #     "PORT": 3306,
+    #     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    
+    "default": {
+       'ENGINE':'django.db.backends.mysql',
+       "NAME": os.environ.get("DB_NAME"),
+       "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASS"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": 3306,
+    }
     
 }
 
