ab4d19cb07c82aa4602f37b3517de0d691770b7e
kamonsak <kamonsak.ba.64@ubu.ac.th>
2024-01-19 10:15:07 +0700

    แก้ไข

--- interview/management/commands/create_user.py
+++ interview/management/commands/create_user.py
@@ -36,7 +36,7 @@ class Command(BaseCommand):
                 )
 
                 new_user_m, created = User.objects.get_or_create(username=user['username'])
-                if new_user_m.user['username'] in '64114540025':
+                if new_user_m.user['username'] in admin:
                     new_user_m.is_staff = True
                 else:
                     new_user_m.is_staff = False
